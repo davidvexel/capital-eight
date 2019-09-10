26948fe57cdbdb128a8f9a72127cb978b7777db8
David Leal <davidvexel@gmail.com>
2019-09-10 18:39:18 -0500

    Working on hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,6 +2,9 @@ import React, { Component } from "react"
 import { Row, Col, Container } from "react-bootstrap"
 import heroBg from "../images/top-image.png"
 import logoCenter from "../images/logo-center-home.png"
+import iconHero1 from "../images/iconHero1.png"
+import iconHero2 from "../images/iconHero2.png"
+import iconHero3 from "../images/iconHero3.png"
 
 export default class Hero extends Component {
   render() {
@@ -14,23 +17,34 @@ export default class Hero extends Component {
       >
         <Container className="text-center">
           <Row>
-            <Col style={{ padding: 30 }}>
+            <Col style={{ padding: 30, marginTop: 200 }}>
               <img src={logoCenter} alt="Logo Home" />
             </Col>
           </Row>
           <Row>
             <Col>
-              <h3>
-                Technology to deliver top performing risk-adjusted returns
-              </h3>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <p>Icons bar</p>
+              <span style={{ fontSize: 28 }}>
+                <b>Technology</b> to deliver top performing risk-adjusted
+                returns
+              </span>
             </Col>
           </Row>
         </Container>
+        <div className="heroIconsBar">
+          <Container className="text-center">
+            <Row>
+              <Col>
+                <img src={iconHero3} alt="" />
+              </Col>
+              <Col>
+                <img src={iconHero2} alt="" />
+              </Col>
+              <Col>
+                <img src={iconHero1} alt="" />
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </div>
     )
   }
